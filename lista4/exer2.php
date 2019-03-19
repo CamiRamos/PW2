d68865e874f6c5a14cbcc4bca84289e18024fdb7
CamiRamos <47786852+CamiRamos@users.noreply.github.com>
2019-03-19 12:30:58 -0300

    Add files via upload

---
+++ lista4/exer2.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>post</title>
+    </head>
+    <body>
+
+        <form action="processa.php" method="Post">
+            
+            1° Número: <input time="number" name="N1">
+            2° Número: <input time="number" name="N2">
+            3° Número: <input time="number" name="N3">
+            4° Número: <input time="number" name="N4">
+            5° Numero: <input time="number" name="N5">
+            
+            <input type="submit"
+                   nam="enviar"
+                   value="Enviar">  
+    </body>
+</html>
---
+++ lista4/soma.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="resultado-soma.php" method="Post">
+  
+        1° Número <input type="number" name="num1">
+        2° Número <input type="number" name="num2">
+          <input type="submit"
+           name="enviar"
+           value="Enviar">
+ </form>
+    </body>
+</html>
+
